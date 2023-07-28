4a1925fbcd0624b2c804bb30deae66798552d49f
Abi <abijay97@gmail.com>
2023-07-28 15:19:30 +0100

    commit 1

---
+++ superheroes/src/main/java/com/example/superheroes/practice/Calculator.java
@@ -0,0 +1,25 @@
+package com.example.superheroes.practice;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Calculator {
+    public int addition(int num1, int num2){
+        return num1+num2;
+    }
+
+    @PostConstruct
+    public void init(){
+        System.out.println("Calculator was created");
+    }
+
+    @PreDestroy
+    public void destroy(){
+        System.out.println("Calculator was destroyed");
+    }
+
+
+
+}
---
+++ superheroes/src/main/resources/data.sql
@@ -0,0 +1,13 @@
+insert into heroes
+    (name, alias, superpower, teamid)
+values
+    ('Bruce', 'Batman', 'MartialArts', 1),
+    ('Max', 'Superman', 'Boxing', 1),
+    ('Alex', 'Flash', 'Stealing', 2),
+    ('Tim', 'AquaMan', 'Fighting', 2),
+    ('Sam', 'Hero12', 'Archery', 3),
+    ('Bob', 'Man12', 'Rowing', 3),
+    ('Bill', 'Bat', 'Running', 4),
+    ('Kate', 'SuperWoman', 'Lifting', 4)
+    ;
+
